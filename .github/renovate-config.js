4f596e274062950c1d1fbeb0daf683facac5d949
Alexandru Nedelcu <noreply@alexn.org>
2026-05-31 09:04:43 +0300

    Add renovate for repo updates

---
+++ .github/renovate-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  platform: "github",
+  repositories: ["funfix/database"],
+  gitAuthor: "Renovate Bot <renovate@funfix.org>",
+  branchPrefix: "renovate/",
+  onboarding: false,
+  requireConfig: "optional",
+
+  extends: ["config:recommended"],
+
+  enabledManagers: ["github-actions", "gradle", "gradle-wrapper", "sbt"],
+
+  ignorePaths: ["**/.gradle/**"],
+
+  packageRules: [
+    {
+      description: "Group all dependency updates into a single PR",
+      matchManagers: ["github-actions", "gradle", "gradle-wrapper", "sbt"],
+      groupName: "dependencies",
+      groupSlug: "all-dependencies",
+      group: {
+        commitMessageTopic: "dependencies",
+        commitMessageExtra: "",
+      },
+    },
+    {
+      description: "Only use stable dotted numeric JVM dependency versions",
+      matchManagers: ["gradle", "gradle-wrapper", "sbt"],
+      allowedVersions: "/^\\d+(?:\\.\\d+)+$/",
+    },
+    {
+      description: "Wait one week before proposing dependency updates",
+      matchManagers: ["github-actions", "gradle", "gradle-wrapper", "sbt"],
+      minimumReleaseAge: "1 days",
+    },
+  ],
+};
